bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacmn: Replace WMI_LOGI() with wmi_* appropriate log level
Replace WMI_LOGI() with wmi_* appropriate log level.

Change-Id: I7b0c32a2aefc5eb300348edbc6a60e7ad0401439
CRs-Fixed: 2774563

wmi/src/wmi_unified_tlv.c
@@ -1071,7 +1071,7 @@ static QDF_STATUS send_vdev_start_cmd_tlv(wmi_unified_t wmi_handle,
 	WMITLV_SET_HDR(buf_ptr, WMITLV_TAG_ARRAY_STRUC,
 		       cmd->num_noa_descriptors *
 		       sizeof(wmi_p2p_noa_descriptor));
-	WMI_LOGI("%s: vdev_id %d freq %d chanmode %d ch_info: 0x%x is_dfs %d "
+	wmi_info("%s: vdev_id %d freq %d chanmode %d ch_info: 0x%x is_dfs %d "
 		"beacon interval %d dtim %d center_chan %d center_freq2 %d "
 		"reg_info_1: 0x%x reg_info_2: 0x%x, req->max_txpow: 0x%x "
 		"Tx SS %d, Rx SS %d, ldpc_rx: %d, cac %d, regd %d, HE ops: %d"
@@ -1857,7 +1857,7 @@ static QDF_STATUS send_wow_enable_cmd_tlv(wmi_unified_t wmi_handle,
 		cmd->pause_iface_config = WOW_IFACE_PAUSE_DISABLED;
 	cmd->flags = param->flags;
 
-	WMI_LOGI("suspend type: %s",
+	wmi_info("suspend type: %s",
 		cmd->pause_iface_config == WOW_IFACE_PAUSE_ENABLED ?
 		"WOW_IFACE_PAUSE_ENABLED" : "WOW_IFACE_PAUSE_DISABLED");
 
@@ -5489,8 +5489,8 @@ static QDF_STATUS send_oem_dma_cfg_cmd_tlv(wmi_unified_t wmi_handle,
 
 	cmd = (uint8_t *) wmi_buf_data(buf);
 	qdf_mem_copy(cmd, cfg, sizeof(*cfg));
-	WMI_LOGI(FL("Sending OEM Data Request to target, data len %lu"),
-		sizeof(*cfg));
+	wmi_debug("Sending OEM Data Request to target, data len %lu"),
+		 sizeof(*cfg);
 	wmi_mtrace(WMI_OEM_DMA_RING_CFG_REQ_CMDID, NO_SESSION, 0);
 	ret = wmi_unified_cmd_send(wmi_handle, buf, sizeof(*cfg),
 				WMI_OEM_DMA_RING_CFG_REQ_CMDID);
@@ -6300,7 +6300,7 @@ send_set_vap_dscp_tid_map_cmd_tlv(wmi_unified_t wmi_handle,
 	cmd->vdev_id = param->vdev_id;
 	cmd->enable_override = 0;
 
-	WMI_LOGI("Setting dscp for vap id: %d", cmd->vdev_id);
+	wmi_debug("Setting dscp for vap id: %d", cmd->vdev_id);
 	wmi_mtrace(WMI_VDEV_SET_DSCP_TID_MAP_CMDID, cmd->vdev_id, 0);
 	if (wmi_unified_cmd_send(wmi_handle, buf, len,
 				 WMI_VDEV_SET_DSCP_TID_MAP_CMDID)) {
@@ -6606,34 +6606,32 @@ static QDF_STATUS send_vdev_spectral_configure_cmd_tlv(wmi_unified_t wmi_handle,
 		wmi_buf_free(buf);
 	}
 
-	WMI_LOGI("%s: Sent WMI_VDEV_SPECTRAL_SCAN_CONFIGURE_CMDID",
-		 __func__);
-
-	WMI_LOGI("vdev_id = %u", param->vdev_id);
-	WMI_LOGI("spectral_scan_count = %u", param->count);
-	WMI_LOGI("spectral_scan_period = %u", param->period);
-	WMI_LOGI("spectral_scan_priority = %u", param->spectral_pri);
-	WMI_LOGI("spectral_scan_fft_size = %u", param->fft_size);
-	WMI_LOGI("spectral_scan_gc_ena = %u", param->gc_enable);
-	WMI_LOGI("spectral_scan_restart_ena = %u", param->restart_enable);
-	WMI_LOGI("spectral_scan_noise_floor_ref = %u", param->noise_floor_ref);
-	WMI_LOGI("spectral_scan_init_delay = %u", param->init_delay);
-	WMI_LOGI("spectral_scan_nb_tone_thr = %u",  param->nb_tone_thr);
-	WMI_LOGI("spectral_scan_str_bin_thr = %u", param->str_bin_thr);
-	WMI_LOGI("spectral_scan_wb_rpt_mode = %u", param->wb_rpt_mode);
-	WMI_LOGI("spectral_scan_rssi_rpt_mode = %u", param->rssi_rpt_mode);
-	WMI_LOGI("spectral_scan_rssi_thr = %u", param->rssi_thr);
-	WMI_LOGI("spectral_scan_pwr_format = %u", param->pwr_format);
-	WMI_LOGI("spectral_scan_rpt_mode = %u", param->rpt_mode);
-	WMI_LOGI("spectral_scan_bin_scale = %u", param->bin_scale);
-	WMI_LOGI("spectral_scan_dBm_adj = %u", param->dbm_adj);
-	WMI_LOGI("spectral_scan_chn_mask = %u", param->chn_mask);
-	WMI_LOGI("spectral_scan_mode = %u", param->mode);
-	WMI_LOGI("spectral_scan_center_freq1 = %u", param->center_freq1);
-	WMI_LOGI("spectral_scan_center_freq2 = %u", param->center_freq2);
-	WMI_LOGI("spectral_scan_chan_freq = %u", param->chan_freq);
-	WMI_LOGI("spectral_scan_chan_width = %u", param->chan_width);
-	WMI_LOGI("%s: Status: %d", __func__, ret);
+	wmi_debug("Sent WMI_VDEV_SPECTRAL_SCAN_CONFIGURE_CMDID");
+	wmi_debug("vdev_id: %u spectral_scan_count: %u",
+		 param->vdev_id, param->count);
+	wmi_debug("spectral_scan_period: %u spectral_scan_priority: %u",
+		 param->period, param->spectral_pri);
+	wmi_debug("spectral_scan_fft_size: %u spectral_scan_gc_ena: %u",
+		 param->fft_size, param->gc_enable);
+	wmi_debug("spectral_scan_restart_ena: %u", param->restart_enable);
+	wmi_debug("spectral_scan_noise_floor_ref: %u", param->noise_floor_ref);
+	wmi_debug("spectral_scan_init_delay: %u", param->init_delay);
+	wmi_debug("spectral_scan_nb_tone_thr: %u", param->nb_tone_thr);
+	wmi_debug("spectral_scan_str_bin_thr: %u", param->str_bin_thr);
+	wmi_debug("spectral_scan_wb_rpt_mode: %u", param->wb_rpt_mode);
+	wmi_debug("spectral_scan_rssi_rpt_mode: %u", param->rssi_rpt_mode);
+	wmi_debug("spectral_scan_rssi_thr: %u spectral_scan_pwr_format: %u",
+		 param->rssi_thr, param->pwr_format);
+	wmi_debug("spectral_scan_rpt_mode: %u spectral_scan_bin_scale: %u",
+		 param->rpt_mode, param->bin_scale);
+	wmi_debug("spectral_scan_dBm_adj: %u spectral_scan_chn_mask: %u",
+		 param->dbm_adj, param->chn_mask);
+	wmi_debug("spectral_scan_mode: %u spectral_scan_center_freq1: %u",
+		 param->mode, param->center_freq1);
+	wmi_debug("spectral_scan_center_freq2: %u spectral_scan_chan_freq: %u",
+		 param->center_freq2, param->chan_freq);
+	wmi_debug("spectral_scan_chan_width: %u Status: %d",
+		 param->chan_width, ret);
 
 	return ret;
 }
@@ -6682,10 +6680,9 @@ static QDF_STATUS send_vdev_spectral_enable_cmd_tlv(wmi_unified_t wmi_handle,
 	}
 	cmd->spectral_scan_mode = param->mode;
 
-	WMI_LOGI("vdev_id = %u", cmd->vdev_id);
-	WMI_LOGI("trigger_cmd = %u", cmd->trigger_cmd);
-	WMI_LOGI("enable_cmd = %u", cmd->enable_cmd);
-	WMI_LOGI("spectral_scan_mode = %u", cmd->spectral_scan_mode);
+	wmi_debug("vdev_id = %u trigger_cmd = %u enable_cmd = %u",
+		 cmd->vdev_id, cmd->trigger_cmd, cmd->enable_cmd);
+	wmi_debug("spectral_scan_mode = %u", cmd->spectral_scan_mode);
 
 	wmi_mtrace(WMI_VDEV_SPECTRAL_SCAN_ENABLE_CMDID, cmd->vdev_id, 0);
 	ret = wmi_unified_cmd_send(wmi_handle, buf, len,
@@ -6696,9 +6693,8 @@ static QDF_STATUS send_vdev_spectral_enable_cmd_tlv(wmi_unified_t wmi_handle,
 		wmi_buf_free(buf);
 	}
 
-	WMI_LOGI("%s: Sent WMI_VDEV_SPECTRAL_SCAN_ENABLE_CMDID", __func__);
-
-	WMI_LOGI("%s: Status: %d", __func__, ret);
+	wmi_debug("Sent WMI_VDEV_SPECTRAL_SCAN_ENABLE_CMDID, Status: %d",
+		  ret);
 
 	return ret;
 }
@@ -7407,7 +7403,7 @@ static QDF_STATUS send_log_supported_evt_cmd_tlv(wmi_unified_t wmi_handle,
 	WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID_param_tlvs *param_buf;
 	wmi_diag_event_log_supported_event_fixed_params *wmi_event;
 
-	WMI_LOGI("Received WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID");
+	wmi_debug("Received WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID");
 
 	param_buf = (WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID_param_tlvs *) event;
 	if (!param_buf) {
@@ -8158,13 +8154,12 @@ static QDF_STATUS send_unit_test_cmd_tlv(wmi_unified_t wmi_handle,
 	WMITLV_SET_HDR(buf_ptr, WMITLV_TAG_ARRAY_UINT32,
 		       (wmi_utest->num_args * sizeof(uint32_t)));
 	unit_test_cmd_args = (uint32_t *) (buf_ptr + WMI_TLV_HDR_SIZE);
-	WMI_LOGI("%s: VDEV ID: %d", __func__, cmd->vdev_id);
-	WMI_LOGI("%s: MODULE ID: %d", __func__, cmd->module_id);
-	WMI_LOGI("%s: TOKEN: %d", __func__, cmd->diag_token);
-	WMI_LOGI("%s: %d num of args = ", __func__, wmi_utest->num_args);
+	wmi_debug("VDEV ID: %d MODULE ID: %d TOKEN: %d",
+		 cmd->vdev_id, cmd->module_id, cmd->diag_token);
+	wmi_debug("%d num of args = ", wmi_utest->num_args);
 	for (i = 0; (i < wmi_utest->num_args && i < WMI_UNIT_TEST_MAX_NUM_ARGS); i++) {
 		unit_test_cmd_args[i] = wmi_utest->args[i];
-		WMI_LOGI("%d,", wmi_utest->args[i]);
+		wmi_debug("%d,", wmi_utest->args[i]);
 	}
 	wmi_mtrace(WMI_UNIT_TEST_CMDID, cmd->vdev_id, 0);
 	if (wmi_unified_cmd_send(wmi_handle, wmi_buf, len,
@@ -8220,10 +8215,10 @@ static QDF_STATUS send_power_dbg_cmd_tlv(wmi_unified_t wmi_handle,
 	WMITLV_SET_HDR(buf_ptr, WMITLV_TAG_ARRAY_UINT32,
 		       (param->num_args * sizeof(uint32_t)));
 	cmd_args = (uint32_t *) (buf_ptr + WMI_TLV_HDR_SIZE);
-	WMI_LOGI("%s: %d num of args = ", __func__, param->num_args);
+	wmi_debug("%d num of args = ", param->num_args);
 	for (i = 0; (i < param->num_args && i < WMI_MAX_POWER_DBG_ARGS); i++) {
 		cmd_args[i] = param->args[i];
-		WMI_LOGI("%d,", param->args[i]);
+		wmi_debug("%d,", param->args[i]);
 	}
 
 	wmi_mtrace(WMI_PDEV_WAL_POWER_DEBUG_CMDID, NO_SESSION, 0);
@@ -8262,7 +8257,7 @@ static QDF_STATUS send_dfs_phyerr_offload_en_cmd_tlv(wmi_unified_t wmi_handle,
 	len = sizeof(*cmd);
 	buf = wmi_buf_alloc(wmi_handle, len);
 
-	WMI_LOGI("%s: pdev_id=%d", __func__, pdev_id);
+	wmi_debug("pdev_id=%d", pdev_id);
 
 	if (!buf)
 		return QDF_STATUS_E_NOMEM;
@@ -8311,7 +8306,7 @@ static QDF_STATUS send_dfs_phyerr_offload_dis_cmd_tlv(wmi_unified_t wmi_handle,
 	len = sizeof(*cmd);
 	buf = wmi_buf_alloc(wmi_handle, len);
 
-	WMI_LOGI("%s: pdev_id=%d", __func__, pdev_id);
+	wmi_debug("pdev_id=%d", pdev_id);
 
 	if (!buf)
 		return QDF_STATUS_E_NOMEM;
@@ -9748,7 +9743,7 @@ static QDF_STATUS extract_host_mem_req_tlv(wmi_unified_t wmi_handle,
 			}
 		}
 
-		WMI_LOGI("idx %d req %d  num_units %d num_unit_info %d"
+		wmi_debug("idx %d req %d  num_units %d num_unit_info %d"
 			 "unit size %d actual units %d",
 			 idx, mem_reqs->req_id,
 			 mem_reqs->num_units,
@@ -10173,12 +10168,12 @@ static QDF_STATUS extract_unit_test_tlv(wmi_unified_t wmi_handle,
 	unit_test->diag_token = ev_param->diag_token;
 	unit_test->flag = ev_param->flag;
 	unit_test->payload_len = ev_param->payload_len;
-	WMI_LOGI("%s:vdev_id:%d mod_id:%d diag_token:%d flag:%d", __func__,
+	wmi_debug("vdev_id:%d mod_id:%d diag_token:%d flag:%d",
 			ev_param->vdev_id,
 			ev_param->module_id,
 			ev_param->diag_token,
 			ev_param->flag);
-	WMI_LOGD("%s: Unit-test data given below %d", __func__, num_bufp);
+	wmi_debug("Unit-test data given below %d", num_bufp);
 	qdf_trace_hex_dump(QDF_MODULE_ID_WMI, QDF_TRACE_LEVEL_DEBUG,
 			bufp, num_bufp);
 	copy_size = (num_bufp < maxspace) ? num_bufp : maxspace;
@@ -11063,7 +11058,7 @@ static QDF_STATUS validate_dbr_ring_caps_idx(uint8_t idx,
 {
 	/* If dma_ring_caps is populated, num_dbr_ring_caps is non-zero */
 	if (!num_dma_ring_caps) {
-		WMI_LOGI("%s: dma_ring_caps %d", __func__, num_dma_ring_caps);
+		wmi_err("dma_ring_caps %d", num_dma_ring_caps);
 		return QDF_STATUS_E_INVAL;
 	}
 	if (idx >= num_dma_ring_caps) {
@@ -11841,7 +11836,7 @@ static QDF_STATUS extract_dfs_radar_detection_event_tlv(
 	radar_found->freq_offset = radar_event->freq_offset;
 	radar_found->sidx = radar_event->sidx;
 
-	WMI_LOGI("processed radar found event pdev %d,"
+	wmi_info("processed radar found event pdev %d,"
 		"Radar Event Info:pdev_id %d,timestamp %d,chan_freq  (dur) %d,"
 		"chan_width (RSSI) %d,detector_id (false_radar) %d,"
 		"freq_offset (radar_check) %d,segment_id %d,sidx %d,"
@@ -13728,7 +13723,7 @@ static QDF_STATUS extract_time_sync_ftm_start_stop_event_tlv(
 	param->mac_time = ((uint64_t)resp_event->mac_timer_u32 << 32) |
 			   resp_event->mac_timer_l32;
 
-	WMI_LOGI("%s: FTM time sync time_interval %d, num_reads %d", __func__,
+	wmi_debug("FTM time sync time_interval %d, num_reads %d",
 		 param->timer_interval, param->num_reads);
 
 	return QDF_STATUS_SUCCESS;

ardia-kun/android_kernel_xiaomi_surya

wmi/src/wmi_unified_tlv.c
@@ -1047,7 +1047,7 @@ static QDF_STATUS send_vdev_start_cmd_tlv(wmi_unified_t wmi_handle,
 	WMITLV_SET_HDR(buf_ptr, WMITLV_TAG_ARRAY_STRUC,
 		       cmd->num_noa_descriptors *
 		       sizeof(wmi_p2p_noa_descriptor));
-	WMI_LOGI("%s: vdev_id %d freq %d chanmode %d ch_info: 0x%x is_dfs %d "
+	wmi_info("%s: vdev_id %d freq %d chanmode %d ch_info: 0x%x is_dfs %d "
 		"beacon interval %d dtim %d center_chan %d center_freq2 %d "
 		"reg_info_1: 0x%x reg_info_2: 0x%x, req->max_txpow: 0x%x "
 		"Tx SS %d, Rx SS %d, ldpc_rx: %d, cac %d, regd %d, HE ops: %d"
@@ -5410,8 +5410,8 @@ static QDF_STATUS send_oem_dma_cfg_cmd_tlv(wmi_unified_t wmi_handle,
 
 	cmd = (uint8_t *) wmi_buf_data(buf);
 	qdf_mem_copy(cmd, cfg, sizeof(*cfg));
-	WMI_LOGI(FL("Sending OEM Data Request to target, data len %lu"),
-		sizeof(*cfg));
+	wmi_debug("Sending OEM Data Request to target, data len %lu"),
+		 sizeof(*cfg);
 	wmi_mtrace(WMI_OEM_DMA_RING_CFG_REQ_CMDID, NO_SESSION, 0);
 	ret = wmi_unified_cmd_send(wmi_handle, buf, sizeof(*cfg),
 				WMI_OEM_DMA_RING_CFG_REQ_CMDID);
@@ -6197,7 +6197,7 @@ send_set_vap_dscp_tid_map_cmd_tlv(wmi_unified_t wmi_handle,
 	cmd->vdev_id = param->vdev_id;
 	cmd->enable_override = 0;
 
-	WMI_LOGI("Setting dscp for vap id: %d", cmd->vdev_id);
+	wmi_debug("Setting dscp for vap id: %d", cmd->vdev_id);
 	wmi_mtrace(WMI_VDEV_SET_DSCP_TID_MAP_CMDID, cmd->vdev_id, 0);
 	if (wmi_unified_cmd_send(wmi_handle, buf, len,
 				 WMI_VDEV_SET_DSCP_TID_MAP_CMDID)) {
@@ -6434,33 +6434,31 @@ static QDF_STATUS send_vdev_spectral_configure_cmd_tlv(wmi_unified_t wmi_handle,
 		wmi_buf_free(buf);
 	}
 
-	WMI_LOGI("%s: Sent WMI_VDEV_SPECTRAL_SCAN_CONFIGURE_CMDID",
-		 __func__);
-
-	WMI_LOGI("vdev_id = %u", param->vdev_id);
-	WMI_LOGI("spectral_scan_count = %u", param->count);
-	WMI_LOGI("spectral_scan_period = %u", param->period);
-	WMI_LOGI("spectral_scan_priority = %u", param->spectral_pri);
-	WMI_LOGI("spectral_scan_fft_size = %u", param->fft_size);
-	WMI_LOGI("spectral_scan_gc_ena = %u", param->gc_enable);
-	WMI_LOGI("spectral_scan_restart_ena = %u", param->restart_enable);
-	WMI_LOGI("spectral_scan_noise_floor_ref = %u", param->noise_floor_ref);
-	WMI_LOGI("spectral_scan_init_delay = %u", param->init_delay);
-	WMI_LOGI("spectral_scan_nb_tone_thr = %u",  param->nb_tone_thr);
-	WMI_LOGI("spectral_scan_str_bin_thr = %u", param->str_bin_thr);
-	WMI_LOGI("spectral_scan_wb_rpt_mode = %u", param->wb_rpt_mode);
-	WMI_LOGI("spectral_scan_rssi_rpt_mode = %u", param->rssi_rpt_mode);
-	WMI_LOGI("spectral_scan_rssi_thr = %u", param->rssi_thr);
-	WMI_LOGI("spectral_scan_pwr_format = %u", param->pwr_format);
-	WMI_LOGI("spectral_scan_rpt_mode = %u", param->rpt_mode);
-	WMI_LOGI("spectral_scan_bin_scale = %u", param->bin_scale);
-	WMI_LOGI("spectral_scan_dBm_adj = %u", param->dbm_adj);
-	WMI_LOGI("spectral_scan_chn_mask = %u", param->chn_mask);
-	WMI_LOGI("spectral_scan_mode = %u", param->mode);
-	WMI_LOGI("spectral_scan_center_freq = %u", param->center_freq);
-	WMI_LOGI("spectral_scan_chan_freq = %u", param->chan_freq);
-	WMI_LOGI("spectral_scan_chan_width = %u", param->chan_width);
-	WMI_LOGI("%s: Status: %d", __func__, ret);
+	wmi_debug("Sent WMI_VDEV_SPECTRAL_SCAN_CONFIGURE_CMDID");
+	wmi_debug("vdev_id: %u spectral_scan_count: %u",
+		 param->vdev_id, param->count);
+	wmi_debug("spectral_scan_period: %u spectral_scan_priority: %u",
+		 param->period, param->spectral_pri);
+	wmi_debug("spectral_scan_fft_size: %u spectral_scan_gc_ena: %u",
+		 param->fft_size, param->gc_enable);
+	wmi_debug("spectral_scan_restart_ena: %u", param->restart_enable);
+	wmi_debug("spectral_scan_noise_floor_ref: %u", param->noise_floor_ref);
+	wmi_debug("spectral_scan_init_delay: %u", param->init_delay);
+	wmi_debug("spectral_scan_nb_tone_thr: %u", param->nb_tone_thr);
+	wmi_debug("spectral_scan_str_bin_thr: %u", param->str_bin_thr);
+	wmi_debug("spectral_scan_wb_rpt_mode: %u", param->wb_rpt_mode);
+	wmi_debug("spectral_scan_rssi_rpt_mode: %u", param->rssi_rpt_mode);
+	wmi_debug("spectral_scan_rssi_thr: %u spectral_scan_pwr_format: %u",
+		 param->rssi_thr, param->pwr_format);
+	wmi_debug("spectral_scan_rpt_mode: %u spectral_scan_bin_scale: %u",
+		 param->rpt_mode, param->bin_scale);
+	wmi_debug("spectral_scan_dBm_adj: %u spectral_scan_chn_mask: %u",
+		 param->dbm_adj, param->chn_mask);
+	wmi_debug("spectral_scan_mode: %u spectral_scan_center_freq: %u",
+		 param->mode, param->center_freq);
+	wmi_debug("spectral_scan_chan_freq: %u", param->chan_freq);
+	wmi_debug("spectral_scan_chan_width: %u Status: %d",
+		 param->chan_width, ret);
 
 	return ret;
 }
@@ -6509,10 +6507,9 @@ static QDF_STATUS send_vdev_spectral_enable_cmd_tlv(wmi_unified_t wmi_handle,
 	}
 	cmd->spectral_scan_mode = param->mode;
 
-	WMI_LOGI("vdev_id = %u", cmd->vdev_id);
-	WMI_LOGI("trigger_cmd = %u", cmd->trigger_cmd);
-	WMI_LOGI("enable_cmd = %u", cmd->enable_cmd);
-	WMI_LOGI("spectral_scan_mode = %u", cmd->spectral_scan_mode);
+	wmi_debug("vdev_id = %u trigger_cmd = %u enable_cmd = %u",
+		 cmd->vdev_id, cmd->trigger_cmd, cmd->enable_cmd);
+	wmi_debug("spectral_scan_mode = %u", cmd->spectral_scan_mode);
 
 	wmi_mtrace(WMI_VDEV_SPECTRAL_SCAN_ENABLE_CMDID, cmd->vdev_id, 0);
 	ret = wmi_unified_cmd_send(wmi_handle, buf, len,
@@ -6523,9 +6520,8 @@ static QDF_STATUS send_vdev_spectral_enable_cmd_tlv(wmi_unified_t wmi_handle,
 		wmi_buf_free(buf);
 	}
 
-	WMI_LOGI("%s: Sent WMI_VDEV_SPECTRAL_SCAN_ENABLE_CMDID", __func__);
-
-	WMI_LOGI("%s: Status: %d", __func__, ret);
+	wmi_debug("Sent WMI_VDEV_SPECTRAL_SCAN_ENABLE_CMDID, Status: %d",
+		  ret);
 
 	return ret;
 }
@@ -7128,7 +7124,7 @@ static QDF_STATUS send_log_supported_evt_cmd_tlv(wmi_unified_t wmi_handle,
 	WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID_param_tlvs *param_buf;
 	wmi_diag_event_log_supported_event_fixed_params *wmi_event;
 
-	WMI_LOGI("Received WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID");
+	wmi_debug("Received WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID");
 
 	param_buf = (WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID_param_tlvs *) event;
 	if (!param_buf) {
@@ -7743,13 +7739,12 @@ static QDF_STATUS send_unit_test_cmd_tlv(wmi_unified_t wmi_handle,
 	WMITLV_SET_HDR(buf_ptr, WMITLV_TAG_ARRAY_UINT32,
 		       (wmi_utest->num_args * sizeof(uint32_t)));
 	unit_test_cmd_args = (uint32_t *) (buf_ptr + WMI_TLV_HDR_SIZE);
-	WMI_LOGI("%s: VDEV ID: %d", __func__, cmd->vdev_id);
-	WMI_LOGI("%s: MODULE ID: %d", __func__, cmd->module_id);
-	WMI_LOGI("%s: TOKEN: %d", __func__, cmd->diag_token);
-	WMI_LOGI("%s: %d num of args = ", __func__, wmi_utest->num_args);
+	wmi_debug("VDEV ID: %d MODULE ID: %d TOKEN: %d",
+		 cmd->vdev_id, cmd->module_id, cmd->diag_token);
+	wmi_debug("%d num of args = ", wmi_utest->num_args);
 	for (i = 0; (i < wmi_utest->num_args && i < WMI_UNIT_TEST_MAX_NUM_ARGS); i++) {
 		unit_test_cmd_args[i] = wmi_utest->args[i];
-		WMI_LOGI("%d,", wmi_utest->args[i]);
+		wmi_debug("%d,", wmi_utest->args[i]);
 	}
 	wmi_mtrace(WMI_UNIT_TEST_CMDID, cmd->vdev_id, 0);
 	if (wmi_unified_cmd_send(wmi_handle, wmi_buf, len,
@@ -7805,10 +7800,10 @@ static QDF_STATUS send_power_dbg_cmd_tlv(wmi_unified_t wmi_handle,
 	WMITLV_SET_HDR(buf_ptr, WMITLV_TAG_ARRAY_UINT32,
 		       (param->num_args * sizeof(uint32_t)));
 	cmd_args = (uint32_t *) (buf_ptr + WMI_TLV_HDR_SIZE);
-	WMI_LOGI("%s: %d num of args = ", __func__, param->num_args);
+	wmi_debug("%d num of args = ", param->num_args);
 	for (i = 0; (i < param->num_args && i < WMI_MAX_POWER_DBG_ARGS); i++) {
 		cmd_args[i] = param->args[i];
-		WMI_LOGI("%d,", param->args[i]);
+		wmi_debug("%d,", param->args[i]);
 	}
 
 	wmi_mtrace(WMI_PDEV_WAL_POWER_DEBUG_CMDID, NO_SESSION, 0);
@@ -7847,7 +7842,7 @@ static QDF_STATUS send_dfs_phyerr_offload_en_cmd_tlv(wmi_unified_t wmi_handle,
 	len = sizeof(*cmd);
 	buf = wmi_buf_alloc(wmi_handle, len);
 
-	WMI_LOGI("%s: pdev_id=%d", __func__, pdev_id);
+	wmi_debug("pdev_id=%d", pdev_id);
 
 	if (!buf)
 		return QDF_STATUS_E_NOMEM;
@@ -7896,7 +7891,7 @@ static QDF_STATUS send_dfs_phyerr_offload_dis_cmd_tlv(wmi_unified_t wmi_handle,
 	len = sizeof(*cmd);
 	buf = wmi_buf_alloc(wmi_handle, len);
 
-	WMI_LOGI("%s: pdev_id=%d", __func__, pdev_id);
+	wmi_debug("pdev_id=%d", pdev_id);
 
 	if (!buf)
 		return QDF_STATUS_E_NOMEM;
@@ -8809,7 +8804,7 @@ static QDF_STATUS extract_host_mem_req_tlv(wmi_unified_t wmi_handle,
 			}
 		}
 
-		WMI_LOGI("idx %d req %d  num_units %d num_unit_info %d"
+		wmi_debug("idx %d req %d  num_units %d num_unit_info %d"
 			 "unit size %d actual units %d",
 			 idx, mem_reqs->req_id,
 			 mem_reqs->num_units,
@@ -9474,12 +9469,12 @@ static QDF_STATUS extract_unit_test_tlv(wmi_unified_t wmi_handle,
 	unit_test->diag_token = ev_param->diag_token;
 	unit_test->flag = ev_param->flag;
 	unit_test->payload_len = ev_param->payload_len;
-	WMI_LOGI("%s:vdev_id:%d mod_id:%d diag_token:%d flag:%d", __func__,
+	wmi_debug("vdev_id:%d mod_id:%d diag_token:%d flag:%d",
 			ev_param->vdev_id,
 			ev_param->module_id,
 			ev_param->diag_token,
 			ev_param->flag);
-	WMI_LOGD("%s: Unit-test data given below %d", __func__, num_bufp);
+	wmi_debug("Unit-test data given below %d", num_bufp);
 	qdf_trace_hex_dump(QDF_MODULE_ID_WMI, QDF_TRACE_LEVEL_DEBUG,
 			bufp, num_bufp);
 	copy_size = (num_bufp < maxspace) ? num_bufp : maxspace;
@@ -10600,7 +10595,7 @@ static QDF_STATUS validate_dbr_ring_caps_idx(uint8_t idx,
 {
 	/* If dma_ring_caps is populated, num_dbr_ring_caps is non-zero */
 	if (!num_dma_ring_caps) {
-		WMI_LOGI("%s: dma_ring_caps %d", __func__, num_dma_ring_caps);
+		wmi_err("dma_ring_caps %d", num_dma_ring_caps);
 		return QDF_STATUS_E_INVAL;
 	}
 	if (idx >= num_dma_ring_caps) {
@@ -11341,7 +11336,7 @@ static QDF_STATUS extract_dfs_radar_detection_event_tlv(
 	radar_found->freq_offset = radar_event->freq_offset;
 	radar_found->sidx = radar_event->sidx;
 
-	WMI_LOGI("processed radar found event pdev %d,"
+	wmi_info("processed radar found event pdev %d,"
 		"Radar Event Info:pdev_id %d,timestamp %d,chan_freq  (dur) %d,"
 		"chan_width (RSSI) %d,detector_id (false_radar) %d,"
 		"freq_offset (radar_check) %d,segment_id %d,sidx %d,"
@@ -13181,7 +13176,7 @@ static QDF_STATUS extract_time_sync_ftm_start_stop_event_tlv(
 	param->mac_time = ((uint64_t)resp_event->mac_timer_u32 << 32) |
 			   resp_event->mac_timer_l32;
 
-	WMI_LOGI("%s: FTM time sync time_interval %d, num_reads %d", __func__,
+	wmi_debug("FTM time sync time_interval %d, num_reads %d",
 		 param->timer_interval, param->num_reads);
 
 	return QDF_STATUS_SUCCESS;