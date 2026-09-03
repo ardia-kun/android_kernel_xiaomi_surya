320b8efc4ce67eb9581930d424b9109d56649989
ardia-kun <ardiayogabekti@gmail.com>
2026-09-03 17:50:56 +0700

    sched/fair: Fast path for idle prev_cpu in find_best_target
    Avoid expensive energy calculation loops when prev_cpu is already idle
    and capable of running the task, significantly reducing wakeup latency
    and idle power consumption.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7949,24 +7949,20 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	if (!sd)
 		return -1;
 
-	/* fast path for prev_cpu */
+	/* fast path for prev_cpu (kerneltoast/Sultan-inspired EAS bypass) */
 	if ((capacity_orig_of(prev_cpu) == capacity_orig_of(cpu)) &&
 		!cpu_isolated(prev_cpu) && cpu_online(prev_cpu) &&
 		idle_cpu(prev_cpu)) {
 
-		if (idle_get_state_idx(cpu_rq(prev_cpu)) <= 1) {
-			/*
-			 * Since target_cpu and backup_cpu are both -1s the
-			 * caller will choose prev_cpu and importantly skip
-			 * energy evaluation
-			 */
-			target_cpu = -1;
-
-			fbt_env->fastpath = PREV_CPU_FASTPATH;
-			trace_sched_find_best_target(p, prefer_idle, min_util,
-					cpu, -1, -1, -1, target_cpu, -1);
-			goto out;
-		}
+		/*
+		 * If prev_cpu is already idle and has enough capacity, reuse it
+		 * immediately to skip expensive energy calculations.
+		 */
+		target_cpu = -1;
+		fbt_env->fastpath = PREV_CPU_FASTPATH;
+		trace_sched_find_best_target(p, prefer_idle, min_util,
+				cpu, -1, -1, -1, target_cpu, -1);
+		goto out;
 	}
 
 	/* Scan CPUs in all SDs */
