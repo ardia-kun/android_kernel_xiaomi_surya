ardia-kun/android_kernel_xiaomi_surya

commit f853cac42c47c989e0c439bde4fa3cda8588990c

surya: fix warnings, errors, and log spam from dmesg audit
Comprehensive fix for issues identified from boot log analysis:
- pinctrl-msm: Guard PDC resource mapping when NULL to avoid 'invalid resource'
- dts: pm8009: Disable absent PM8009 PMIC and RPMh regulators on surya
- dts: audio: Disable unsupported senary TDM DAIs
- dts: audio: Remove non-existent WSA speaker routes
- asoc: sm6150: Only set ignore_suspend on WSA widgets if aux devs exist
- fsa4480-i2c: Bail out gracefully with -ENODEV if chip is not present
- i2c-qcom-geni: Downgrade -ENOTCONN (NACK) logging to GENI_SE_DBG
- aw8624: Increase post-reset delay and avoid false errors before retries
- qcacld-3.0: Refcount mibstats_lock to prevent mutex collision on p2p0
- qcacld-3.0: Add QDF_NAN_DISC_MODE (mode 16) handling to SME and PMO
- qcacld-3.0: Match wlan_hdd_send_hang_reason_event prototype to definition
- qca-wifi-host-cmn: Downgrade initial wake up log from ATH_DEBUG_ANY to TRC
- qca-wifi-host-cmn: Downgrade CONFIG_SHADOW_V2 not defined to debug
- probe deferral: Filter -EPROBE_DEFER from error prints across drivers
- power: bq2597x: Downgrade suspend/resume success messages to bq_dbg
- camera: wl2866d: Change suspend/resume trace printk from pr_err to pr_debug
- of: batterydata: Change battery profile found message to pr_debug
- devfreq: bimc-bwmon: Change zone threshold warning to pr_debug
- net: sysctl: Change ip_local_port_range parity error to warn
- scm: Downgrade SCM call failure to pr_debug and default QHEE prot to n
- diag: Downgrade PCIe MHI ops registration failure to pr_debug
- kernelsu: Silence zygote handle_setresuid, overlimit cert, and sepolicy noise
- techpack: audio: Fix format specifier mismatches and cal_data array check

Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

arch/arm64/boot/dts/qcom/pm8009.dtsi
@@ -18,6 +18,7 @@
 		reg = <0xa SPMI_USID>;
 		#address-cells = <1>;
 		#size-cells = <1>;
+		status = "disabled";
 
 		qcom,revid@100 {
 			compatible = "qcom,qpnp-revid";
@@ -35,6 +36,7 @@
 			gpio-controller;
 			#gpio-cells = <2>;
 			qcom,gpios-disallowed = <3>;
+			status = "disabled";
 		};
 	};
 
@@ -43,5 +45,6 @@
 		reg = <0xb SPMI_USID>;
 		#address-cells = <1>;
 		#size-cells = <1>;
+		status = "disabled";
 	};
 };

arch/arm64/boot/dts/qcom/sdmmagpie-regulator.dtsi
@@ -159,6 +159,7 @@
 	};
 
 	rpmh-regulator-smpf1 {
+		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "smpf1";
@@ -172,6 +173,7 @@
 	};
 
 	rpmh-regulator-smpf2 {
+		status = "disabled";
 		compatible = "qcom,rpmh-xob-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "smpf2";
@@ -782,6 +784,7 @@
 	};
 
 	rpmh-regulator-ldof1 {
+		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof1";
@@ -801,6 +804,7 @@
 	};
 
 	rpmh-regulator-ldof2 {
+		status = "disabled";
 		compatible = "qcom,rpmh-xob-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof2";
@@ -813,6 +817,7 @@
 	};
 
 	rpmh-regulator-ldof4 {
+		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof4";
@@ -832,6 +837,7 @@
 	};
 
 	rpmh-regulator-ldof5 {
+		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof5";
@@ -851,6 +857,7 @@
 	};
 
 	rpmh-regulator-ldof6 {
+		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof6";
@@ -870,6 +877,7 @@
 	};
 
 	rpmh-regulator-ldof7 {
+		status = "disabled";
 		compatible = "qcom,rpmh-xob-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof7";

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1104,8 +1104,9 @@ static void gpio_muxed_to_pdc(struct irq_domain *pdc_domain, struct irq_data *d)
 			/* setup the IRQ parent for the GPIO */
 			setup_pdc_gpio(pctrl->chip.irqdomain, irq, gpio);
 			/* program pdc select grp register */
-			writel_relaxed((mux & 0x3F), pctrl->pdc_regs +
-				(0x14 * j));
+			if (pctrl->pdc_regs)
+				writel_relaxed((mux & 0x3F), pctrl->pdc_regs +
+					(0x14 * j));
 			break;
 		}
 		/* We have no more PDC port available */
@@ -2127,7 +2128,10 @@ int msm_pinctrl_probe(struct platform_device *pdev,
 
 	key = "pdc";
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, key);
-	pctrl->pdc_regs = devm_ioremap_resource(&pdev->dev, res);
+	if (res)
+		pctrl->pdc_regs = devm_ioremap_resource(&pdev->dev, res);
+	else
+		pctrl->pdc_regs = NULL;
 
 	key = "spi_cfg";
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, key);

drivers/regulator/qpnp-lcdb-regulator.c
@@ -2340,7 +2340,8 @@ static int qpnp_lcdb_regulator_probe(struct platform_device *pdev)
 
 	rc = qpnp_lcdb_parse_dt(lcdb);
 	if (rc < 0) {
-		pr_err("Failed to parse dt rc=%d\n", rc);
+		if (rc != -EPROBE_DEFER)
+			pr_err("Failed to parse dt rc=%d\n", rc);
 		return rc;
 	}
 

drivers/soc/qcom/Kconfig
@@ -952,7 +952,7 @@ config QMP_DEBUGFS_CLIENT
 config QCOM_QHEE_ENABLE_MEM_PROTECTION
 	bool "QHEE enable kernel memory protection"
 	depends on QCOM_SCM
-	default y
+	default n
 	help
 	  When this option is enabled, an SCM call will be invoked to enable
 	  kernel memory protection in stage 2 memory mappings on kernel boot.

drivers/soc/qcom/fsa4480-i2c.c
@@ -350,13 +350,18 @@ static void fsa4480_usbc_analog_work_fn(struct work_struct *work)
 	pm_relax(fsa_priv->dev);
 }
 
-static void fsa4480_update_reg_defaults(struct regmap *regmap)
+static int fsa4480_update_reg_defaults(struct regmap *regmap)
 {
 	u8 i;
+	int rc;
 
-	for (i = 0; i < ARRAY_SIZE(fsa_reg_i2c_defaults); i++)
-		regmap_write(regmap, fsa_reg_i2c_defaults[i].reg,
+	for (i = 0; i < ARRAY_SIZE(fsa_reg_i2c_defaults); i++) {
+		rc = regmap_write(regmap, fsa_reg_i2c_defaults[i].reg,
 				   fsa_reg_i2c_defaults[i].val);
+		if (rc)
+			return rc;
+	}
+	return 0;
 }
 
 static int fsa4480_probe(struct i2c_client *i2c,
@@ -393,7 +398,13 @@ static int fsa4480_probe(struct i2c_client *i2c,
 		goto err_supply;
 	}
 
-	fsa4480_update_reg_defaults(fsa_priv->regmap);
+	rc = fsa4480_update_reg_defaults(fsa_priv->regmap);
+	if (rc) {
+		dev_dbg(fsa_priv->dev, "%s: device not detected: %d\n",
+			__func__, rc);
+		rc = -ENODEV;
+		goto err_supply;
+	}
 
 	fsa_priv->psy_nb.notifier_call = fsa4480_usbc_event_changed;
 	fsa_priv->psy_nb.priority = 0;

drivers/soc/qcom/scm.c
@@ -1295,10 +1295,10 @@ int scm_enable_mem_protection(void)
 	}
 
 	if (ret == -1) {
-		pr_err("%s: SCM call not supported\n", __func__);
+		pr_debug("%s: SCM call not supported\n", __func__);
 		return ret;
 	} else if (ret || resp) {
-		pr_err("%s: SCM call failed\n", __func__);
+		pr_debug("%s: SCM call failed\n", __func__);
 		if (ret)
 			return ret;
 		else

arch/arm64/boot/dts/qcom/msm-audio-lpass.dtsi
@@ -764,6 +764,7 @@
 	};
 
 	tdm_sen_rx: qcom,msm-dai-tdm-sen-rx {
+		status = "disabled";
 		compatible = "qcom,msm-dai-tdm";
 		qcom,msm-cpudai-tdm-group-id = <37200>;
 		qcom,msm-cpudai-tdm-group-num-ports = <1>;
@@ -783,6 +784,7 @@
 	};
 
 	tdm_sen_tx: qcom,msm-dai-tdm-sen-tx {
+		status = "disabled";
 		compatible = "qcom,msm-dai-tdm";
 		qcom,msm-cpudai-tdm-group-id = <37201>;
 		qcom,msm-cpudai-tdm-group-num-ports = <1>;

commit b85c25c5a501bfee466ac3f12289c068b3384455

surya: fix blank screen caused by fsa4480 probe failure and regulator disabling
Commit f853cac42c47 introduced changes to fsa4480-i2c, regulators, and pinctrl
that resulted in a blank screen on boot:

1. fsa4480-i2c: Returning -ENODEV when fsa4480_update_reg_defaults() fails
   prevented i2c_set_clientdata() from being initialized. Consequently,
   dp_display_init_aux_switch() in drivers/gpu/drm/msm/dp/dp_display.c failed
   fsa4480_reg_notifier() and deferred probe (-EPROBE_DEFER) forever. Because
   dp_display never added its DRM component (component_add), the MSM DRM master
   driver never initialized the display subsystem, resulting in a blank screen.
   Restore fsa4480_probe to gracefully proceed without aborting on initial
   register update failure.

2. DTS & Regulators: Disabling pm8009 and rpmh-regulator-smpf*/ldof* nodes in
   pm8009.dtsi and sdmmagpie-regulator.dtsi caused consumer phandle lookups to
   fail or defer. Revert these DT and Kconfig/scm/pinctrl changes.

Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

arch/arm64/boot/dts/qcom/pm8009.dtsi
@@ -18,7 +18,6 @@
 		reg = <0xa SPMI_USID>;
 		#address-cells = <1>;
 		#size-cells = <1>;
-		status = "disabled";
 
 		qcom,revid@100 {
 			compatible = "qcom,qpnp-revid";
@@ -36,7 +35,6 @@
 			gpio-controller;
 			#gpio-cells = <2>;
 			qcom,gpios-disallowed = <3>;
-			status = "disabled";
 		};
 	};
 
@@ -45,6 +43,5 @@
 		reg = <0xb SPMI_USID>;
 		#address-cells = <1>;
 		#size-cells = <1>;
-		status = "disabled";
 	};
 };

arch/arm64/boot/dts/qcom/sdmmagpie-regulator.dtsi
@@ -159,7 +159,6 @@
 	};
 
 	rpmh-regulator-smpf1 {
-		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "smpf1";
@@ -173,7 +172,6 @@
 	};
 
 	rpmh-regulator-smpf2 {
-		status = "disabled";
 		compatible = "qcom,rpmh-xob-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "smpf2";
@@ -784,7 +782,6 @@
 	};
 
 	rpmh-regulator-ldof1 {
-		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof1";
@@ -804,7 +801,6 @@
 	};
 
 	rpmh-regulator-ldof2 {
-		status = "disabled";
 		compatible = "qcom,rpmh-xob-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof2";
@@ -817,7 +813,6 @@
 	};
 
 	rpmh-regulator-ldof4 {
-		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof4";
@@ -837,7 +832,6 @@
 	};
 
 	rpmh-regulator-ldof5 {
-		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof5";
@@ -857,7 +851,6 @@
 	};
 
 	rpmh-regulator-ldof6 {
-		status = "disabled";
 		compatible = "qcom,rpmh-vrm-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof6";
@@ -877,7 +870,6 @@
 	};
 
 	rpmh-regulator-ldof7 {
-		status = "disabled";
 		compatible = "qcom,rpmh-xob-regulator";
 		mboxes = <&apps_rsc 0>;
 		qcom,resource-name = "ldof7";

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1104,9 +1104,8 @@ static void gpio_muxed_to_pdc(struct irq_domain *pdc_domain, struct irq_data *d)
 			/* setup the IRQ parent for the GPIO */
 			setup_pdc_gpio(pctrl->chip.irqdomain, irq, gpio);
 			/* program pdc select grp register */
-			if (pctrl->pdc_regs)
-				writel_relaxed((mux & 0x3F), pctrl->pdc_regs +
-					(0x14 * j));
+			writel_relaxed((mux & 0x3F), pctrl->pdc_regs +
+				(0x14 * j));
 			break;
 		}
 		/* We have no more PDC port available */
@@ -2128,10 +2127,7 @@ int msm_pinctrl_probe(struct platform_device *pdev,
 
 	key = "pdc";
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, key);
-	if (res)
-		pctrl->pdc_regs = devm_ioremap_resource(&pdev->dev, res);
-	else
-		pctrl->pdc_regs = NULL;
+	pctrl->pdc_regs = devm_ioremap_resource(&pdev->dev, res);
 
 	key = "spi_cfg";
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, key);

drivers/regulator/qpnp-lcdb-regulator.c
@@ -2340,8 +2340,7 @@ static int qpnp_lcdb_regulator_probe(struct platform_device *pdev)
 
 	rc = qpnp_lcdb_parse_dt(lcdb);
 	if (rc < 0) {
-		if (rc != -EPROBE_DEFER)
-			pr_err("Failed to parse dt rc=%d\n", rc);
+		pr_err("Failed to parse dt rc=%d\n", rc);
 		return rc;
 	}
 

drivers/soc/qcom/Kconfig
@@ -952,7 +952,7 @@ config QMP_DEBUGFS_CLIENT
 config QCOM_QHEE_ENABLE_MEM_PROTECTION
 	bool "QHEE enable kernel memory protection"
 	depends on QCOM_SCM
-	default n
+	default y
 	help
 	  When this option is enabled, an SCM call will be invoked to enable
 	  kernel memory protection in stage 2 memory mappings on kernel boot.

drivers/soc/qcom/fsa4480-i2c.c
@@ -350,18 +350,13 @@ static void fsa4480_usbc_analog_work_fn(struct work_struct *work)
 	pm_relax(fsa_priv->dev);
 }
 
-static int fsa4480_update_reg_defaults(struct regmap *regmap)
+static void fsa4480_update_reg_defaults(struct regmap *regmap)
 {
 	u8 i;
-	int rc;
 
-	for (i = 0; i < ARRAY_SIZE(fsa_reg_i2c_defaults); i++) {
-		rc = regmap_write(regmap, fsa_reg_i2c_defaults[i].reg,
+	for (i = 0; i < ARRAY_SIZE(fsa_reg_i2c_defaults); i++)
+		regmap_write(regmap, fsa_reg_i2c_defaults[i].reg,
 				   fsa_reg_i2c_defaults[i].val);
-		if (rc)
-			return rc;
-	}
-	return 0;
 }
 
 static int fsa4480_probe(struct i2c_client *i2c,
@@ -398,13 +393,7 @@ static int fsa4480_probe(struct i2c_client *i2c,
 		goto err_supply;
 	}
 
-	rc = fsa4480_update_reg_defaults(fsa_priv->regmap);
-	if (rc) {
-		dev_dbg(fsa_priv->dev, "%s: device not detected: %d\n",
-			__func__, rc);
-		rc = -ENODEV;
-		goto err_supply;
-	}
+	fsa4480_update_reg_defaults(fsa_priv->regmap);
 
 	fsa_priv->psy_nb.notifier_call = fsa4480_usbc_event_changed;
 	fsa_priv->psy_nb.priority = 0;

drivers/soc/qcom/scm.c
@@ -1295,10 +1295,10 @@ int scm_enable_mem_protection(void)
 	}
 
 	if (ret == -1) {
-		pr_debug("%s: SCM call not supported\n", __func__);
+		pr_err("%s: SCM call not supported\n", __func__);
 		return ret;
 	} else if (ret || resp) {
-		pr_debug("%s: SCM call failed\n", __func__);
+		pr_err("%s: SCM call failed\n", __func__);
 		if (ret)
 			return ret;
 		else